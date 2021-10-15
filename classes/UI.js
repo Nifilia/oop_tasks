118b660dfd3ed4fe10a68985b871c5adad3cbd90
Nifilia <Anderokore@gmail.com>
2021-10-15 12:57:27 +0300

    fixed typos in ui

--- classes/UI.js
+++ classes/UI.js
@@ -1,10 +1,10 @@
 class UI{
 	addTask(task){
-		const li = document.creatElement("li");
+		const li = document.createElement("li");
 		li.className = "collection-item";
 		li.appendChild(document.createTextNode(task.name));
 
-		const link = document.creatElement("a");
+		const link = document.createElement("a");
 		link.className = "secondary-content";
 		link.appendChild(document.createTextNode("X"));
 		link.setAttribute("href", "#");
