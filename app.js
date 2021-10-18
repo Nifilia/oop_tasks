667efee00457d04bbfb6bf48a7fda31900f2e371
Nifilia <Anderokore@gmail.com>
2021-10-18 11:45:28 +0300

    lesson

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ ls = new LS();
 const form = document.querySelector('form');
 const taskInput = document.querySelector("#task");
 
+const taskList = document.querySelector("ul");
+taskList.addEventListener("click", deleteTask);
+
+const clearBtn = document.querySelector("#clear-tasks");
+clearBtn.addEventListener("click", deleteTasks);
+
+document.addEventListener("DOMContentLoaded", getTasks);
+
+
 form.addEventListener('submit', addTask);
 
 function addTask(e){
@@ -11,4 +20,21 @@ function addTask(e){
 	ui.addTask(task);
 	ls.addTask(task);
 	e.preventDefault();
+}
+function deleteTask(e){
+	let task = e.target.parentElement.firstChild;
+	ui.deleteTask(task);
+	task = task.textContent;
+	ls.deleteTask(task);
+}
+
+function deleteTasks(e){
+	let tasks = document.querySelector("ul");
+	ui.deleteTasks(tasks);
+	ls.deleteTasks();
+}
+
+function getTasks(e){
+	tasks = ls.getData("tasks");
+	ui.getTasks(tasks);
 }
